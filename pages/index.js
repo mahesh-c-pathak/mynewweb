6961df508c6b09919e60d6aa7bff69b03aa20f2e
Mahesh <mahesh.c.pathak@gmail.com>
2023-04-09 09:02:21 +0530

    the next 1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,26 @@
 export default function Home() {
+  const createTest = async () => {
+    const randomNum = Math.floor(Math.random() * 1000);
+    const res = await fetch('/api/test/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: `Test ${randomNum}`,
+        email: `test${randomNum}@test.com`,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
-    <h1>My new web</h1>
- 
+    <div>
+      <h1>My new web</h1>
+      <br />
+      <button onClick={createTest}>Create Test</button>
+
+    </div> 
   )
 }
